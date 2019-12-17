9f3828166ba138c044d5f591138b989c06bfe516
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2019-12-17 17:02:13 +0400

    fix copy chown

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ ! $1 ]]; then
-    echo "Usage: /copy USER_ID:USER_GROUP_ID"
-    exit 1
-fi
-
 if [[ ! -d /dist ]]; then
     mkdir /dist
 fi
@@ -15,4 +10,6 @@ rm -rf /dist/*
 
 cp /xdebug/modules/xdebug.so /dist/xdebug.so
 
-chown -R "$1" /dist
+if [[ ! $1 ]]; then
+    chown -R "$1" /dist
+fi
