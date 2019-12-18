998ef229c1153c8694ad58591208b09bf65bceae
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2019-12-18 12:10:46 +0400

    example php base app

---
+++ examples/base-php-app/docker-app-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- php-fpm "$@"
+fi
+
+if [ ! -z "$XDEBUG_CONFIG" ]; then
+    XDEBUG_SO_PATH="/usr/local/lib/php/extensions/xdebug.so"
+    wget -O "$XDEBUG_SO_PATH" https://github.com/danilin-em/xdebug-builder/releases/download/v2.9.0/xdebug.so
+    set -- "$@" -d "zend_extension=$XDEBUG_SO_PATH"
+fi
+
+exec "$@"
---
+++ examples/base-php-app/src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$mydate = date('Y-m-d');
+
+echo "Hello World!\n";
+
+echo "My Data is: ${mydate}\n";
